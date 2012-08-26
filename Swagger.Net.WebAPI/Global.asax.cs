d41514fdc91007f40906ec5149644fc38aab41ef
Mike Trionfo <mike.trionfo@payvio.com>
2012-08-26 15:08:18 -0600

    Added a new RTM WebApi project

---
+++ Swagger.Net.WebAPI/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Swagger.Net.WebApi
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Configure_Swagger();
+        }
+
+        private void Configure_Swagger()
+        {
+            var config = GlobalConfiguration.Configuration;
+
+            // The Web API Configuration Object
+            // Remove the XML Formatter
+            var xmlFormatter = config.Formatters
+              .Where(f =>
+              {
+                  return f.SupportedMediaTypes.Any(v => v.MediaType == "text/xml");
+              })
+              .FirstOrDefault();
+
+            if (xmlFormatter != null)
+            {
+                config.Formatters.Remove(xmlFormatter);
+            }
+        }
+    }
+}
