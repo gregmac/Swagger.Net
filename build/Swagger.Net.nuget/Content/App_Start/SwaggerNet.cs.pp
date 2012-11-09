3c2a142484dcff7050d68d2dbb4efc8da3210ac0
gregmac <github@list.mtechsolutions.ca>
2012-11-09 01:19:45 -0500

    Add msbuild and nuget spec files for main Swagger.Net project, and add artifact dirs to gitignore file

---
+++ build/Swagger.Net.nuget/Content/App_Start/SwaggerNet.cs.pp
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Dispatcher;
+using System.Web.Routing;
+using Swagger.Net;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SwaggerNet), "PreStart")]
+namespace $rootnamespace$.App_Start
+{
+    public static class SwaggerNet
+    {
+        public static void PreStart()
+        {
+            RouteTable.Routes.MapHttpRoute(
+                name: "SwaggerResourceList",
+                routeTemplate: "apidocs/swagger",
+                defaults: new { swagger = true, controller = "Swagger", action = "GetResourceList" }
+            );
+            RouteTable.Routes.MapHttpRoute(
+                name: "SwaggerApiDeclaration",
+                routeTemplate: "apidocs/{controllerName}",
+                defaults: new { swagger = true, controller = "Swagger", action = "GetApiDeclaration" }
+            );
+
+            SwaggerConfiguration.DefaultConfiguration = SwaggerConfiguration.CreateDefaultConfig(GlobalConfiguration.Configuration);
+        }
+    }
+}
